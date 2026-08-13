9e7f2967474398fb43269df8e85a34c8bf8faeb6
umaiiier <umairnaseer7788@gmail.com>
2026-08-13 17:13:05 +0500

    Candidate Name added as input

--- program.cpp
+++ program.cpp
@@ -3,7 +3,11 @@ using namespace std;
 int main() {
    
 cout << "Pakistan College of Technology" << endl;    
+cout << "==============================" << endl;
 
+string name;
+cout << "Enter your name: ";
+cin >> name;
 
 
 }
