0323328d413a23c8fd37382f9305dad768a2d44d
umaiiier <umairnaseer7788@gmail.com>
2026-08-14 07:03:25 +0500

    Some changes in Logic

--- program.cpp
+++ program.cpp
@@ -17,15 +17,11 @@ int matric_Percentage;
 cout << "Enter your Matric Percentage: ";
 cin >> matric_Percentage;
 
-if (intermediate_Percentage >= 50)
-    cout << "You are eligible for admission." << endl;
-       
-if (matric_Percentage >= 50)
-    cout << "You are eligible for admission." << endl;
+if (intermediate_Percentage >= 50 && matric_Percentage >= 50)
+    cout << "Congratulations! " << name << " , You are eligible for admission." << endl;
 
 else
-   
-    cout << "You are not eligible for admission." << endl;
+    cout << "Sorry, " << name << " , You are not eligible for admission." << endl;
 
 
 }
