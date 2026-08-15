be07248922e48ecf9f31c8b3619c9b737ccd0b16
umaiiier <umairnaseer7788@gmail.com>
2026-08-15 09:34:06 +0500

    Add entry test marks to admission eligibility

--- program.cpp
+++ program.cpp
@@ -1,27 +1,36 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-   
-cout << "Pakistan College of Technology" << endl;    
-cout << "==============================" << endl;
 
-string name;
-cout << "Enter your name: ";
-cin >> name;
+    cout << "Pakistan College of Technology" << endl;
+    cout << "==============================" << endl;
 
-int intermediate_Percentage;
-cout << "Enter your Intermediate Percentage: ";
-cin >> intermediate_Percentage;
+    string name;
+    cout << "Enter your name: ";
+    cin >> name;
 
-int matric_Percentage;
-cout << "Enter your Matric Percentage: ";
-cin >> matric_Percentage;
+    int intermediate_Percentage;
+    cout << "Enter your Intermediate Percentage: ";
+    cin >> intermediate_Percentage;
 
-if (intermediate_Percentage >= 50 && matric_Percentage >= 50)
-    cout << "Congratulations! " << name << " , You are eligible for admission." << endl;
+    int matric_Percentage;
+    cout << "Enter your Matric Percentage: ";
+    cin >> matric_Percentage;
 
-else
-    cout << "Sorry, " << name << " , You are not eligible for admission." << endl;
+    int entryTest_Marks;
+    cout << "Enter your Entry Test Marks: ";
+    cin >> entryTest_Marks;
 
+    if (intermediate_Percentage >= 50 &&
+        matric_Percentage >= 50 &&
+        entryTest_Marks >= 50)
+
+        cout << "Congratulations! " << name
+             << ", You are eligible for admission." << endl;
+
+    else
+        cout << "Sorry, " << name
+             << ", You are not eligible for admission." << endl;
 
 }
