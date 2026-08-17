e96aff3e32e427ff969b658cd2d88fa4d01d979b
umaiiier <umairnaseer7788@gmail.com>
2026-08-17 07:09:06 +0500

    Add input validation for admission criteria

--- program.cpp
+++ program.cpp
@@ -14,13 +14,28 @@ int main() {
     cout << "Enter your Intermediate Percentage: ";
     cin >> intermediate_Percentage;
 
+    while (intermediate_Percentage < 0 || intermediate_Percentage > 100) {
+        cout << "Invalid percentage! Please enter a value between 0 and 100: ";
+        cin >> intermediate_Percentage;
+    }
+
     int matric_Percentage;
     cout << "Enter your Matric Percentage: ";
     cin >> matric_Percentage;
 
+    while (matric_Percentage < 0 || matric_Percentage > 100) {
+        cout << "Invalid percentage! Please enter a value between 0 and 100: ";
+        cin >> matric_Percentage;
+    }
+
     int entryTest_Marks;
     cout << "Enter your Entry Test Marks: ";
     cin >> entryTest_Marks;
+
+    while (entryTest_Marks < 0 || entryTest_Marks > 100) {
+        cout << "Invalid marks! Please enter a value between 0 and 100: ";
+        cin >> entryTest_Marks;
+    }
 
     if (intermediate_Percentage >= 50 &&
         matric_Percentage >= 50 &&
